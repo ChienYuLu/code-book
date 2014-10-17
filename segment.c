7a1d3ae64ebcbbd15484796354467243485d8960
Gordon <geniusgordon@gmail.com>
2014-10-17 19:58:36 +0800

    segment tree fixed

--- segment.c
+++ segment.c
@@ -30,8 +30,8 @@ void down(int L, int R, int M, int t)
 int query(int L, int R, int x, int y, int t)
 {
 	if (y <= L || R <= x)
-		return;
-	if (L <= x && y <= R) {
+		return 0;
+	if (x <= L && R <= y) {
 		return tree[t];
 	} else {
 		int M = (L+R) / 2;
@@ -48,7 +48,7 @@ void update(int L, int R, int x, int y, int value, int t)
 {
 	if (y <= L || R <= x)
 		return;
-	if (L <= x && y <= R) {
+	if (x <= L && R <= y) {
 		lazy[t] += value;
 		tree[t] += (R-L)*value;
 	} else {
@@ -57,6 +57,7 @@ void update(int L, int R, int x, int y, int value, int t)
 			down(L, R, M, t);
 		update(L, M, x, y, value, t*2);
 		update(M, R, x, y, value, t*2+1);
+		tree[t] = tree[t*2] + tree[t*2+1];
 	}
 }
 
@@ -70,12 +71,9 @@ int main()
 	build(0, n, 1);
 	
 	update(0, n, 4, 9, 5, 1);
-	printf("%d\n", query(0, n, 4, 9, 1));
-	printf("%d\n", query(0, n, 1, 6, 1));
-	
 	update(0, n, 2, 6, 5, 1);
-	printf("%d\n", query(0, n, 4, 9, 1));
-	printf("%d\n", query(0, n, 7, 9, 1));
+	printf("%d\n", query(0, n, 2, 5, 1));
+	printf("%d\n", query(0, n, 5, 6, 1));
 
 	return 0;
 }
